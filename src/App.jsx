be0adbeb64b0487b3f11998425c2aafc9beb169d
Bishnu <caflebishnuprasad1@gmail.com>
2025-06-03 12:25:18 +0545

    .............

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
   const [count1, setCount1] = useState(0);
 
-  return (
-    <>
-      <div>
-        <button onClick={() => setCount(count + 1)}>Count: {count}</button>
-      </div>
-      <div>
-        <button onClick={() => setCount1(count1 + 1)}>Count1: {count1}</button>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
