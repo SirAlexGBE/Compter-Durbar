8af8c507f65520ee952a267f4f01eb667212ac69
Bishnu <caflebishnuprasad1@gmail.com>
2025-06-03 15:40:16 +0545

    fatal : not a git repository

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import Footer from "../Components/Footer";
 const Home = () => {
   return (
     <>
-      <MacBookProductListing />
+      {/* <MacBookProductListing /> */}
       <News />
       <Footer />
     </>
