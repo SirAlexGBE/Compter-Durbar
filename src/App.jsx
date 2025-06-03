af032fb237bb47c9ee487618163a920cb8b98d29
tysion <tysion@gmail.com>
2025-06-03 12:59:51 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./Component/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
