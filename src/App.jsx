0a803cd4ca0b5d8c2576a84bd0994042ec2bea52
tysion <tysion@gmail.com>
2025-06-03 15:30:29 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./Component/Navbar";
+import { Laptop } from "lucide-react";
+import LaptopCarousel from "./Component/LaptopCarousel";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar />
+      <div className="gap-4 flex flex-col items-center justify-center min-h-screen ">
+        <Navbar />
+        <LaptopCarousel />
+      </div>
     </>
   );
 }
