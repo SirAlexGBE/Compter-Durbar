e344694077bdaa9465e46dcfb7aba764e507bacc
Bishnu <caflebishnuprasad1@gmail.com>
2025-06-03 12:36:52 +0545

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./Pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
