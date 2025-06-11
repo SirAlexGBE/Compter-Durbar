0e1759c0133916dea95519e77d2584cbc27b469f
SirAlexGBE <Bindasjake14@Gmail.com>
2025-06-11 13:47:43 +0545

    newsletter made

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import ProductsOnSale from "../Components/Home/ProductOnsale";
 import DealOfTheDay from "../Components/Home/DealOfTheDay";
 import NewProducts from "../Components/Home/NewProducts";
 import ProductCategories from "../Components/Home/ProductCatogories";
+import NewsletterSubscription from "../Components/Home/Newsletter";
 
 const Home = () => {
   return (
@@ -12,7 +13,8 @@ const Home = () => {
       <DealOfTheDay />
       <NewProducts />
       <ProductCategories />
-      <ProductsOnSale />
+      {/* <ProductsOnSale /> */}
+      <NewsletterSubscription />
     </>
   );
 };
