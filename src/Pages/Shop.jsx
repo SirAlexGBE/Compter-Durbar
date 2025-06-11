e662529e96ce346718b59a25fc8a20728320e6c9
tysion <ctit23.ala@ismt.edu.np>
2025-06-11 14:34:09 +0545

    add data

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -52,11 +52,83 @@ export default function Shop() {
       originalPrice: "Rs 152,999",
       image: "/Newproduct1.png",
     },
+    {
+      id: 7,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 8,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 9,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 10,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 11,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 12,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 13,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 14,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 15,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
   ];
 
   return (
     <div className="w-full mx-auto  py-6">
-      <div className="flex flex-col lg:flex-row items-start gap-6">
+      <div className="flex flex-col lg:flex-row items-start ">
         {/* Sidebar */}
         <div className="w-full lg:w-1/4">
           <FilterComponent />
