5e105d3684eb5a91d17a956e3b0aa46fb86bf828
tysion <ctit23.ala@ismt.edu.np>
2025-06-11 14:29:22 +0545

    change in shop

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,76 @@
 import React from "react";
 import FilterComponent from "../Components/Shop/FilterSidebar";
+import ProductCard from "../Components/Home/ProductCard";
 
 export default function Shop() {
+  const products = [
+    {
+      id: 1,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 2,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 3,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 4,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 5,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+    {
+      id: 6,
+      name: "Lenovo LOQ i7-12650H RTX 4050",
+      specs: "| 16GB RAM | 512GB SSD | 144Hz Display | Luna Grey",
+      currentPrice: "Rs 119,999",
+      originalPrice: "Rs 152,999",
+      image: "/Newproduct1.png",
+    },
+  ];
+
   return (
-    <>
-      <FilterComponent />
-    </>
+    <div className="w-full mx-auto  py-6">
+      <div className="flex flex-col lg:flex-row items-start gap-6">
+        {/* Sidebar */}
+        <div className="w-full lg:w-1/4">
+          <FilterComponent />
+        </div>
+
+        {/* Product Grid */}
+        <div className="w-full lg:w-3/4">
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 ">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
